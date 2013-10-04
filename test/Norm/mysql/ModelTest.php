a578c33cc0301f8066456afb0dd5444369543606
Krisan Alfa Timur <krisan47@gmail.com>
2013-10-04 19:58:21 +0700

    Adding MySQL adapter

---
+++ test/Norm/mysql/ModelTest.php
@@ -0,0 +1,85 @@
+<?php
+
+use Norm\Norm;
+use Norm\Connection;
+use Norm\Connection\MysqlConnection;
+use Norm\Model;
+use Norm\Collection;
+
+class ModelTest extends \PHPUnit_Framework_TestCase {
+
+    private static $connection;
+    private static $collection;
+    private static $db;
+    private static $model;
+
+    // public static function setUpBeforeClass() {
+
+    // }
+
+    public static function setUpBeforeClass() {
+        if (self::$collection) {
+            return;
+        }
+
+        var_dump("Initializing environment...");
+        $config = array(
+            'mysql' => array(
+                'driver' => '\\Norm\\Connection\\MysqlConnection',
+                'database' => 'test',
+                'username' => 'root',
+                'password' => 'password',
+            ),
+        );
+
+        Norm::init($config);
+
+        self::$connection = Norm::getConnection();
+
+        self::$db = self::$connection->getDB();
+
+        self::$collection = Norm::factory('Users');
+
+        $collectionName = self::$collection->name;
+
+        $drop = self::$db->exec("DELETE FROM $collectionName");
+
+        self::$model = self::$collection->newInstance();
+    }
+
+    public function testQuery() {
+        $this->assertTrue(self::$collection instanceof Collection, 'is Norm::factory() returns Collection instance');
+        $a = self::$connection->listCollections();
+        $this->assertTrue(is_array(array()));
+    }
+
+    public function testInsert() {
+        self::$model->set('name', 'adoel');
+        self::$model->set('hobby', 'hiking');
+        self::$model->set('age', '22');
+
+        $this->assertEquals(count(self::$model->save()), 1, 'is able to get array of Model instances');
+    }
+
+    public function testUpdate() {
+        self::$model->set('hobby', 'jogging');
+        self::$model->save();
+
+        $this->assertEquals(self::$model->get('hobby'), 'jogging', 'is able to get array of Model instances');
+    }
+
+    public function testRemove() {
+        $model = self::$collection->findOne(array('name' => 'adoel'));
+
+        $this->assertNotNull($model, 'is null after deleted');
+
+        $model->remove();
+
+        $model = self::$collection->findOne(array(
+            'name' => 'adoel'
+        ));
+
+        $this->assertNull($model, 'is null after deleted');
+    }
+
+}
