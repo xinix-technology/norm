bf03e1eb2c15cfa558c6cd5970c614038e57f4d9
Ganesha <reekoheek@gmail.com>
2014-02-26 14:57:38 +0700

    string representation of norm type datetime

--- src/Norm/Type/DateTime.php
+++ src/Norm/Type/DateTime.php
@@ -3,7 +3,13 @@
 namespace Norm\Type;
 
 class DateTime extends \DateTime implements \JsonKit\JsonSerializer {
+
     public function jsonSerialize() {
         return $this->format('c');
     }
+
+    public function __toString() {
+        return $this->format('c');
+    }
+
 }
