5c826574be6d8b393704903fd46f3868357c0a4b
Krisan Alfa Timur <krisan47@gmail.com>
2014-03-17 20:44:04 +0700

    Count fixing

--- src/Norm/Cursor.php
+++ src/Norm/Cursor.php
@@ -10,6 +10,8 @@ class Cursor implements ICursor,  \JsonKit\JsonSerializer {
 
     protected $collection;
 
+    protected $links;
+
     public function __construct($cursor, $collection) {
         $this->cursor = $cursor;
         $this->collection = $collection;
@@ -73,6 +75,43 @@ class Cursor implements ICursor,  \JsonKit\JsonSerializer {
     public function skip($num) {
         $this->cursor->skip($num);
         return $this;
+    }
+
+    public function paginate($page, $itemsPerPage = null) {
+        if (is_null($page)) {
+            $page = 1;
+        }
+
+        $page = (int) $page;
+
+        if (is_null($itemsPerPage)) {
+            $itemsPerPage = $this->collection->options['pagination'];
+        }
+
+        $totalItems   = $this->count();
+        $totalPage    = round($totalItems / $itemsPerPage);
+        $skip         = ($page > 1) ? ((($page - 1) * $itemsPerPage) + 1) : 0;
+        $urls         = array();
+        $limit        = ($skip > 0) ? ($skip + $itemsPerPage) : $itemsPerPage;
+
+        $urls[] = array('skip' => 0, 'limit' => $itemsPerPage, 'active' => ($page == 1));
+
+        for ($i = 1; $i < $totalPage; $i++) {
+            $urls[$i] = array(
+                'skip' => (($i * $itemsPerPage) + 1),
+                'limit' => $itemsPerPage,
+                'active' => (($page - 1) == $i)
+            );
+        }
+
+        $this->links = $urls;
+
+
+        return $this->skip($skip)->limit($itemsPerPage);
+    }
+
+    public function links() {
+        return $this->links;
     }
 
     public function jsonSerialize() {
