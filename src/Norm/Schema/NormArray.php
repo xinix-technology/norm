62fb8c657557cb049bee11f6b111ea4d8199a7da
januar <januar@januar-K46CM.(none)>
2014-03-18 22:34:53 +0700

    handle empty value for normarray

--- src/Norm/Schema/NormArray.php
+++ src/Norm/Schema/NormArray.php
@@ -6,6 +6,10 @@ class NormArray extends Field {
 
     public function prepare($value) {
 
+    	if(empty($value)){
+    		return '';
+    	}
+
         if (is_string($value)) {
             $value = json_decode($value);
         }
