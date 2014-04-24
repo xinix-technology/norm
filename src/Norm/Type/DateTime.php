b8a3270eaa830bf7812e1939073bcd5ebe0d4923
Ganesha <reekoheek@gmail.com>
2014-04-24 13:51:06 +0700

    add format methods

--- src/Norm/Type/DateTime.php
+++ src/Norm/Type/DateTime.php
@@ -4,6 +4,18 @@ namespace Norm\Type;
 
 class DateTime extends \DateTime implements \JsonKit\JsonSerializer
 {
+    public function localFormat($format)
+    {
+        return $this->tzFormat($format);
+    }
+
+    public function tzFormat($format, $tz = null)
+    {
+        if (isset($tz)) {
+            $this->setTimezone(new \DateTimeZone($tz));
+        }
+        return $this->format($format);
+    }
 
     public function jsonSerialize()
     {
