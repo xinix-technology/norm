94b766e144286e6cba51286e4c90864741ba020f
Ganesha <reekoheek@gmail.com>
2014-06-17 19:31:28 +0700

    updated Field schema will sanitize input on prepare

--- src/Norm/Schema/String.php
+++ src/Norm/Schema/String.php
@@ -6,6 +6,6 @@ class String extends Field
 {
     public function prepare($value)
     {
-        return utf8_encode($value);
+        return utf8_encode(filter_var($value, FILTER_SANITIZE_STRING));
     }
 }
