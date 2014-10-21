8cc2e51f833fb957c7f74b9afef191abe07a4667
Ganesha <reekoheek@gmail.com>
2014-10-22 00:23:39 +0700

    allow collection resolver to specify collection path as option in string or closured value

--- src/Norm/Resolver/CollectionResolver.php
+++ src/Norm/Resolver/CollectionResolver.php
@@ -4,10 +4,33 @@ namespace Norm\Resolver;
 
 class CollectionResolver
 {
+
+    protected $options = array(
+        'collectionPath' => null,
+    );
+
+    public function __construct($options) {
+        $this->options = array_merge($this->options, $options);
+    }
+
     public function resolve($options)
     {
         $app = \App::getInstance();
-        $configPath = rtrim($app->config('config.path'), '/').'/collections/'.$options['name'].'.php';
+
+        $configPath = null;
+
+        if (isset($this->options['collectionPath'])) {
+            if (is_callable($this->options['collectionPath'])) {
+                $configPath = call_user_func($this->options['collectionPath']);
+            } else {
+                $configPath = $this->options['collectionPath'];
+            }
+        }
+
+        if (!is_readable($configPath)) {
+            $configPath = rtrim($app->config('config.path'), '/').'/collections/'.$options['name'].'.php';
+        }
+
         if (is_readable($configPath)) {
             return include($configPath);
         }
