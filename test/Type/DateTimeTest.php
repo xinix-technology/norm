651c8ac1e3fd58ef2c598652f1414b479acda485
Ganesha <reekoheek@gmail.com>
2015-10-27 02:00:30 +0700

    fixed unittest datetimetest to show utc time

--- test/Type/DateTimeTest.php
+++ test/Type/DateTimeTest.php
@@ -11,7 +11,11 @@ class DateTimeTest extends PHPUnit_Framework_TestCase
         $dt = new DateTime();
 
         $this->assertEquals(date('H'), $dt->tzFormat('H'));
-        $this->assertEquals(date('H') -7, $dt->tzFormat('H', 'UTC'));
+        $d = date('H') - 7;
+        if ($d < 0) {
+            $d += 24;
+        }
+        $this->assertEquals($d, $dt->tzFormat('H', 'UTC'));
     }
 
     public function testLocalFormat()
